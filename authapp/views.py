deeaf0a7827bed3b958f8863ab22689d7f570cad
geekbrains <ayankeav@gmail.com>
2021-03-03 22:55:14 +0300

    lesn4  do all, except base.html for login.html and register.html, but I will definitely do it.

--- authapp/views.py
+++ authapp/views.py
@@ -3,7 +3,7 @@ from django.contrib import auth
 from django.urls import reverse
 from django.contrib import messages
 
-from authapp.form import UserLoginForm, UserCreationForm
+from authapp.form import UserLoginForm, UserRegisterForm
 
 # Create your views here.
 def login(request):
@@ -22,4 +22,16 @@ def login(request):
     return render(request, 'authapp/login.html', context)
 
 def register(request):
-    return render(request, 'authapp/register.html')
+    if request.method == 'POST':
+        form = UserRegisterForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('auth:login'))
+    else:
+        form = UserRegisterForm()
+        context = {'form': form}
+    return render(request, 'authapp/register.html', context)
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
