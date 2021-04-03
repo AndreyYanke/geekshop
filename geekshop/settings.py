ea327f95b37fa89690653276add443ed6d84f733
geekbrains <ayankeav@gmail.com>
2021-04-03 09:15:47 +0300

    leson 2 done

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os, json
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -147,9 +148,18 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     'social_core.backends.google.GoogleOAuth2',
 )
+
+
+SOCIAL_SECRETS_FILE = "google.json"
+SOCIAL = {}
+if os.path.exists(SOCIAL_SECRETS_FILE):
+    with open(SOCIAL_SECRETS_FILE, 'r')as f:
+        SOCIAL = json.load(f)
+
+
+
 SESSION_COOKIE_SECURE = True
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = SOCIAL.get('SOCIAL_AUTH_GOOGLE_OAUTH2_KEY', "")
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = SOCIAL.get('SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET', "")
 
-
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = ('405145454375-0f5qsvc2a9npvs0pomb6a002pf722om2.apps.googleusercontent.com')
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = ('0fiOr3on4VvkoT88_TXHczVV')
 
